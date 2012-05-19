fc9a0c95ed1a3cea55a6b02c56daf90d18e836fb
Mike Ledger <eleventynine@gmail.com>
2012-05-19 15:13:18 +1000

    remove dist

---
+++ src/Text/demo.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+import qualified Data.Text.IO as T
+import Text.QuasiText
+
+main :: IO ()
+main = do
+    user  <- T.getLine
+    email <- T.getLine
+    airSpeedVelocityOfAnUnladenSwallow <- fmap read getLine :: IO Int
+    T.putStrLn [embed|
+        The air speed velocity of an unladen swallow is $airSpeedVelocityOfAnUnladenSwallow.
+        Your username is $user.
+        Your email is $email.
+        1 + 1 = $(1 + 1).
+    |]
