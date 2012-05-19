cd6b3e9fafaccbd8e2fa5a81b946a6ce3baaf7cb
Mike Ledger <eleventynine@gmail.com>
2012-05-19 14:32:09 +1000

    more efficient, less packing/unpacking

--- src/Text/QuasiText.hs
+++ src/Text/QuasiText.hs
@@ -3,9 +3,13 @@ module Text.QuasiText (embed, Chunk (..), getChunks) where
 import Language.Haskell.TH.Quote
 import Language.Haskell.TH.Syntax
 import Language.Haskell.TH
-import Data.Attoparsec.Text hiding (take)
-import Data.Text (Text, pack, unpack)
 import Language.Haskell.Meta (parseExp)
+
+import Data.Attoparsec.Text
+import Data.Text as T (Text, pack, unpack, append, empty, head)
+
+instance Lift Text where
+    lift t = litE (stringL (unpack t))
 
 data Chunk = 
       T Text
@@ -13,34 +17,38 @@ data Chunk =
     | V Text
   deriving (Show, Eq)
 
-class Stringy a where
-    toString :: a -> String
+class Textish a where
+    toText :: a -> Text
 
-instance Stringy Text where
-    toString = unpack
+instance Textish Text where
+    toText = id
 
-instance Stringy [Char] where
-    toString x = x
+instance Textish [Char] where
+    toText = pack
 
-instance Show a => Stringy a where 
-    toString = show
+instance Show a => Textish a where 
+    toText = pack . show
 
 -- | A simple 'QuasiQuoter' to interpolate 'Text' into other pieces of 'Text'. 
--- Escape variables using $var, expressions in $(expression)
--- Expressions that are not 'Text' must be directly convertable to it, ie be 'String', or have an instance of 'Show'.
+-- Expressions can be embedded using $(...) or $..., $... will only work for one-word expressions (best suited for just
+-- variable substitution), but $(...) will work for anything..
 embed :: QuasiQuoter
 embed = QuasiQuoter
     { quoteExp = \s -> 
-        let chunks = 
-                flip map (getChunks (pack s)) $ \c ->
+        let chunks = flip map (getChunks (pack s)) $ \c ->
                     case c of
-                        T t -> let str = unpack t in [| str |]
-                        E t -> let str      = unpack t
-                                   Right e  = parseExp str
-                                in appE [| toString |] (return e)
-                        V t -> appE [| toString |] $ global $ mkName $ unpack t
-            unpacked = foldr (\l r -> appE (appE [| (++) |] l) r) [| [] |] chunks
-        in appE [| pack |] unpacked
+                        -- literal text
+                        T t -> [| t |]
+
+                        -- haskell expression
+                        E t -> let Right e = parseExp (unpack t) in appE [| toText |] (return e) 
+
+                        -- one-word expression
+                        V t | T.head t `elem` ['a'..'z'] -> appE [| toText |] (global (mkName (unpack t)))
+                            | otherwise -> let Right e = parseExp (unpack t) in appE [| toText |] (return e)
+
+        in foldr (\l r -> appE (appE [| append |] l) r) [| empty |] chunks
+
     , quotePat  = error "cannot use this as a pattern"
     , quoteDec  = error "cannot use this as a declaration"
     , quoteType = error "cannot use this as a type"
@@ -56,13 +64,13 @@ getChunks i = let Right m = parseOnly parser i in m
         evt <- choice [expression, var, fmap T takeText]
         end <- atEnd
         if end
-            then return $ filter (not . empty) $ reverse (evt:T txt:s)
+            then return $ filter (not . blank) $ reverse (evt:T txt:s)
             else go (evt:T txt:s)
 
-    empty (T "") = True
-    empty (E "") = True
-    empty (V "") = True
-    empty _      = False
+    blank (T "") = True
+    blank (E "") = True
+    blank (V "") = True
+    blank _      = False
 
     var = do
         char '$'
